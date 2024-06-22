d58accea590a48035b905e8e833122fccfe77941
Devysson Cardoso <devyssonsc@gmail.com>
2024-06-23 00:18:44 +0100

    feat: navbar com hamburger

---
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-profile-page',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss'
+})
+export class ProfilePageComponent {
+
+}
