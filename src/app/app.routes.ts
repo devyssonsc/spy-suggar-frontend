5ccff2335ff9c8c00422f6411be82b02ef642670
Devysson Cardoso <devyssonsc@gmail.com>
2024-06-22 20:50:58 +0100

    feat: redirect from empty path to /login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,11 @@ import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+    },
+    {
         path: 'login',
         component: LoginComponent,
     },
