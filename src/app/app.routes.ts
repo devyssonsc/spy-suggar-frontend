46d1e545af6d3b40634e69028dab0dd2b181fb24
Devysson Cardoso <devyssonsc@gmail.com>
2024-07-20 15:04:02 +0100

    feat: cria component stats e adiciona path para acessar component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path: 'profile',
         component: ProfilePageComponent,
+    },
+    {
+        path: 'stats',
+        component: StatsComponent
     }
 ];
