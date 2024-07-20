6eef02c92bca650db76fca1aeb8e532a9156747d
Devysson Cardoso <devyssonsc@gmail.com>
2024-07-20 14:51:49 +0100

    navbar no topo da página

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-form-register',
   standalone: true,
   imports: [
     ReactiveFormsModule,
+    RouterModule,
   ],
   templateUrl: './form-register.component.html',
   styleUrls: ['./form-register.component.scss'],
 })
 export class FormRegisterComponent implements OnInit {
   registerForm!: FormGroup;
+  router: any;
+  route: any;
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
       confirmPassword: ['', [Validators.required]]
