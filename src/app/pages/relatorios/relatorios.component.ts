974553727bd54ca92a5410e99121aebcd34904ec
Devysson Cardoso <devyssonsc@gmail.com>
2024-07-21 05:42:08 +0100

    feat: cria componente modelo para cada relatório

--- src/app/pages/relatorios/relatorios.component.ts
+++ src/app/pages/relatorios/relatorios.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ReportComponent } from '../../components/report/report.component';
 
 @Component({
   selector: 'app-relatorios',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, ReportComponent],
   templateUrl: './relatorios.component.html',
   styleUrl: './relatorios.component.scss'
 })
